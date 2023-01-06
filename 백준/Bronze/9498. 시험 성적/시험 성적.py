0dbe16e4b791ebfcb0a442917691a98fc4b07e46
Gomjong <84004572+Gomjong@users.noreply.github.com>
2023-01-06 14:55:04 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score = int(input())
+if 100>=score>=90:
+    print("A")
+elif 89>=score>=80:
+    print("B")
+elif 79>=score>=70:
+    print("C")
+elif 69>=score>=60:
+    print("D")
+else:
+    print("F")
