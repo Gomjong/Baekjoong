51311f9d521eec4b0e3343ffd5b1a3bd4888b813
Gomjong <84004572+Gomjong@users.noreply.github.com>
2023-01-17 11:33:21 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 376 ms, Memory: 58424 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,8 @@
+import sys
+n = int(sys.stdin.readline())
+so = []
+for i in range(n):
+    so.append(list(map(int, sys.stdin.readline().split())))
+so.sort(key=lambda x: (x[1], x[0]))
+for i in so:
+    print(i[0], i[1])
