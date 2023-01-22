fc2b5287d35c5db18c60b846c0d94ccae49e82a1
Gomjong <84004572+Gomjong@users.noreply.github.com>
2023-01-22 16:31:17 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,8 +1,5 @@
 n = int(input())
 test = list(map(int, input().split()))
 gara = max(test)
-sum = 0
-
-for i in range(len(test)):
-    sum = sum + test[i]/gara*100
-print(sum/n)
+test_sum = sum(test)
+print(test_sum*100/gara/n)
